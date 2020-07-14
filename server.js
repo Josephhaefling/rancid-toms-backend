6d1b5f3536407da4a1161177e862a54ddc2b2810
Alex Eickelman <aeickelman40@gmail.com>
2020-07-14 16:41:24 -0600

    Add cors requirement and additional path for comments per unique movie

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { request, response} = require('express')
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Rancid-Toms';
@@ -8,9 +10,11 @@ app.get('/', (request, response) => {
     response.send("Hi Alex");
 });
 
-app.locals.comments = [
-    { id: 123456, author: 'Jessica', comment: 'this movie is the worst!!!!!' },
-];
+app.locals.comments = {
+    comments: [
+        { id: 123456, author: 'Jessica', comment: 'this movie is the worst!!!!!' },
+    ]
+};
 
 app.locals.favorites = [
     { id: 4 },
@@ -20,6 +24,11 @@ app.get('/comments', (request, response) => {
     const comments = app.locals.comments;
     response.json({ comments })
 });
+
+app.get('/comments/:id', (request, response) => {
+    const comments = app.locals.comments;
+    response.json({ comments })
+})
 
 app.get('/favorites', (request, response) => {
     const favorites = app.locals.favorites
